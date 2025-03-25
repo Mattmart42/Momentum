e61bfd1343d4253f9558fdcae57709397e46db85
Matt Martinez <mattmartinez64@icloud.com>
2025-03-25 11:04:52 -0700

    initial commit

---
+++ momentum/momentum/momentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  momentumApp.swift
+//  momentum
+//
+//  Created by Matthew Martinez on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct momentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ momentum/momentumTests/momentumTests.swift
@@ -0,0 +1,17 @@
+//
+//  momentumTests.swift
+//  momentumTests
+//
+//  Created by Matthew Martinez on 3/25/25.
+//
+
+import Testing
+@testable import momentum
+
+struct momentumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
